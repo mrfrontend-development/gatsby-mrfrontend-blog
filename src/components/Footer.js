8433d6d71ccd46a299c1bb0c369ef0a1d0a64d5a
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2019-06-01 20:48:30 +0200

    Added footer image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,9 @@ import React from 'react'
 import { css } from '@emotion/core'
 import { bpMaxSM } from '../lib/breakpoints'
 // import SubscribeForm from './Forms/Subscribe'
-import { Twitter, GitHub } from './Social'
+import { Twitter, GitHub, Slack } from './Social'
 import Container from './Container'
+import * as banner from '../../static/images/write-for-mr-frontend.jpeg'
 
 const Footer = ({ author, noSubscribeForm }) => (
   <footer>
@@ -15,6 +16,16 @@ const Footer = ({ author, noSubscribeForm }) => (
         }
       `}
     >
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <img
+          src={banner}
+          alt="Write for Mr Frontend"
+          style={{ marginBottom: 0 }}
+        />
+        <a href="https://twitter.com/frontendmr/">
+          <strong>Send a message via Twitter @mrfrontend</strong>
+        </a>
+      </div>
       {/* {!noSubscribeForm && (
         <div>
           <SubscribeForm />
@@ -40,6 +51,7 @@ const Footer = ({ author, noSubscribeForm }) => (
         <div>
           <Twitter />
           <GitHub />
+          <Slack />
         </div>
       </div>
     </Container>
