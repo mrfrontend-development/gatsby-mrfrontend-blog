b3eaa913c1731db29f878d56ec62f2d3f7187fdb
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2019-06-02 16:02:04 +0200

    Added anchors for subtitles

--- src/components/mdx/index.js
+++ src/components/mdx/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import dashify from 'dashify'
 import Title from './Title'
 import Subtitle from './Subtitle'
 import Paragraph from './Paragraph'
@@ -7,7 +7,10 @@ import Code from './Code'
 
 export default {
   h1: props => <Title {...props} />,
-  h2: props => <Subtitle {...props} />,
+  h2: props => {
+    const { children } = props
+    return <Subtitle id={dashify(children)} {...props} />
+  },
   p: props => <Paragraph {...props} />,
   code: Code,
   pre: preProps => <pre {...preProps} />,
