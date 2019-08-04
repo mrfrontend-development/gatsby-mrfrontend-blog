b22246b35032cc23a2d29dc8a3385478a53ede0a
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2019-08-04 13:36:18 +0200

    Add PWA functionalities

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,42 @@ module.exports = {
     'gatsby-plugin-catch-links',
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+        importWorkboxFrom: `local`,
+        globDirectory: './',
+        modifyUrlPrefix: {
+          // If `pathPrefix` is configured by user, we should replace
+          // the default prefix with `pathPrefix`.
+          '/': `${pathPrefix}/`,
+        },
+        cacheId: `gatsby-plugin-offline`,
+        // Don't cache-bust JS or CSS files, and anything in the static directory,
+        // since these files have unique URLs and their contents will never change
+        dontCacheBustUrlsMatching: /(\.js$|\.css$|static\/)/,
+        runtimeCaching: [
+          {
+            // Use cacheFirst since these don't need to be revalidated (same RegExp
+            // and same reason as above)
+            urlPattern: /(\.js$|\.css$|static\/)/,
+            handler: `cacheFirst`,
+          },
+          {
+            // Add runtime caching of various other page resources
+            urlPattern: /^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/,
+            handler: `staleWhileRevalidate`,
+          },
+          {
+            // Google Fonts CSS (doesn't end in .css so we need to specify it)
+            urlPattern: /^https?:\/\/fonts\.googleapis\.com\/css/,
+            handler: `staleWhileRevalidate`,
+          },
+        ],
+        skipWaiting: true,
+        clientsClaim: true,
+      },
+    },
+    {
       resolve: 'gatsby-plugin-manifest',
       options: {
         name: config.siteTitle,
@@ -152,6 +188,5 @@ module.exports = {
         pathToConfigModule: `src/lib/typography`,
       },
     },
-    'gatsby-plugin-offline',
   ],
 }
